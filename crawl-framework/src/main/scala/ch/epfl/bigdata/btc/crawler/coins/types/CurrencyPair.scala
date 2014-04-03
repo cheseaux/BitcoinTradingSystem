ba4b83d109ad624ab33827fac5cbd8a2858e5725
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-04-03 13:08:58 +0200

    added new class CurrencyPair

---
+++ crawl-framework/src/main/scala/ch/epfl/bigdata/btc/crawler/coins/types/CurrencyPair.scala
@@ -0,0 +1,11 @@
+package ch.epfl.bigdata.btc.crawler.coins.types
+
+import ch.epfl.bigdata.btc.crawler.coins.types.Currency._
+
+case class CurrencyPair(c1: Currency, c2: Currency) {
+  override def equals(o: Any) = o match {
+    case that: CurrencyPair => ((c1 == that.c1 && c2 == that.c2) || (c1 == that.c2 && c2 == that.c1))
+    case _ => false
+  }
+  override def hashCode = {c1.hashCode() + c2.hashCode()}
+}
