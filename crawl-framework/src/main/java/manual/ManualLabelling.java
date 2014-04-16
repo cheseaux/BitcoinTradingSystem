55a9d54204ec0a9a9e6c04a4f19d95ce5b04435c
cheseaux <cheseauxjonathan@gmail.com>
2014-04-16 19:55:26 +0200

    added manual labeling system

---
+++ crawl-framework/src/main/java/manual/ManualLabelling.java
@@ -0,0 +1,70 @@
+package manual;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ManualLabelling {
+
+	private static int countLines(File file) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		int count = 0;
+		while(br.readLine() != null) {count++;}
+		br.close();
+		return count;
+	}
+	
+	private static void printLabel(String path, String tweet) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path), true));
+		bw.write(tweet+System.lineSeparator());
+		bw.close();
+	}
+	
+	public static void main(String[] args) {
+		File toClassify = new File("tweet_<name>");
+		Scanner scanner = new Scanner(System.in);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(toClassify));
+			String line = "";
+			int count = 0;
+			int total = countLines(toClassify);
+			while((line = br.readLine()) != null) {
+				boolean cont = false;
+				while(!cont) {
+					cont = true;
+					System.out.println("#### Tweet " + count++ + " out of " + total + ". ####");
+					System.out.println(line);
+					System.out.println("POSITIVE (1) ? NEUTRAL/IRRELEVANT (2) ? POSITIVE (3) ?");
+					int choice = scanner.nextInt();
+					scanner.nextLine();
+					switch (choice) {
+					case 1:
+						printLabel("postive_tweets.txt", line);
+						break;
+					case 2:
+						printLabel("neutral_tweets.txt", line);
+						break;
+					case 3:
+						printLabel("negative_tweets.txt", line);
+						break;
+					default:
+						cont = false;
+						System.err.println("Press 1, 2 or 3 !");
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
