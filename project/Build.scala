6e4706d8e0cb3493a6245ec516640547c1caad1e
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-04-30 19:52:01 +0200

    Build dependency change

--- project/Build.scala
+++ project/Build.scala
@@ -4,5 +4,5 @@ import Keys._
 object SbtMultiBuild extends Build {
     lazy val parent = Project(id = "bts", base = file(".")) aggregate(common, crawl)
     lazy val common = Project(id = "bts-lib-common", base = file("lib-common"))
-    lazy val crawl = Project(id = "bts-crawl", base = file("crawl-framework")).dependsOn(common)
+    lazy val crawl = Project(id = "bts-crawl", base = file("crawl-framework")).dependsOn(common % "compile->package")
 }
