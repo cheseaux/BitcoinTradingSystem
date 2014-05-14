2cbf4e383ee8414be08351b7b56b1a625e453b2b
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-05-14 19:43:31 +0200

    EMA extension with percent

--- crawl-framework/src/main/scala/ch/epfl/bigdata/btc/crawler/coins/DataSource.scala
+++ crawl-framework/src/main/scala/ch/epfl/bigdata/btc/crawler/coins/DataSource.scala
@@ -7,6 +7,7 @@ import ch.epfl.bigdata.btc.types.Transfer._
 import ch.epfl.bigdata.btc.types.Registration._
 import ch.epfl.bigdata.btc.types.CurrencyPair
 import ch.epfl.bigdata.btc.crawler.coins.indicators.EMA
+import ch.epfl.bigdata.btc.crawler.coins.indicators.SMA 
 import scala.collection.mutable.MutableList
 import scala.collection.mutable.HashMap
 import akka.event.Logging
@@ -46,8 +47,15 @@ class DataSource() extends Actor {
           ir match {
             case er: EMARegistration => {
               var indicator = context.actorOf(Props(classOf[EMA], self,
-                MarketPairRegistrationOHLC(er.market, er.c, er.tickSize, er.tickCount), 100, 0.8),
-                er.market.toString + "_" + er.c.c1 + "-" + er.c.c2 + "-" + er.tickSize + "-" + er.tickCount + "100-0.8")
+                MarketPairRegistrationOHLC(er.market, er.c, er.tickSize, er.tickCount), er.tickCount, er.percent),
+                "EMA" + er.market.toString + "_" + er.c.c1 + "-" + er.c.c2 + "-" + er.tickSize + "-" + er.tickCount + "_" + er.tickCount + "-" + er.percent)
+                registrations.addIndicator(ir, indicator)
+                indicator ! observer
+            }
+            case er: SMARegistration => {
+              var indicator = context.actorOf(Props(classOf[SMA], self,
+                MarketPairRegistrationOHLC(er.market, er.c, er.tickSize, er.tickCount), er.tickCount),
+                "SMA" + er.market.toString + "_" + er.c.c1 + "-" + er.c.c2 + "-" + er.tickSize + "-" + er.tickCount)
                 registrations.addIndicator(ir, indicator)
                 indicator ! observer
             }
